db422b74e06947ad3a92a7b89837a80ee1e55135
vaughanstech <vaughanstech@gmail.com>
2022-09-02 15:35:33 -0400

    created tab for transformers-api project

---
+++ src/pages/projects/transformer-api.md
@@ -0,0 +1,9 @@
+---
+layout: "../../templates/BasePost.astro"
+title: Transformers API
+description: Fun API used for including your favorite 'robots in disguise' in your project
+pubDate: 2022-09-03T00:00:00Z
+imgSrc: "/Optimus-Prime-PNG-Clipart.png"
+---
+
+# Hello World
--- src/partials/ProjectsList.tsx
+++ src/partials/ProjectsList.tsx
@@ -22,19 +22,19 @@ const ProjectList = () => (
   >
     <div className="flex flex-col gap-6">
       <Project
-        name="Project 1"
-        description="dummy text for description"
-        link="/"
+        name="Transformers API"
+        description="Fun API used for including your favorite 'robots in disguise' in your project"
+        link="http://localhost:3000/projects/transformer-api"
         img={{
-          src: "/",
+          src: "../../public/transformers-logo.png",
           alt: "Image related to project",
         }}
         category={
           <>
-            <Tags color={ColorTags.FUCHSIA}>Astro.js</Tags>
-            <Tags color={ColorTags.LIME}>Web Design</Tags>
-            <Tags color={ColorTags.SKY}>Tailwind.css</Tags>
-            <Tags color={ColorTags.ROSE}>TypeScript</Tags>
+            <Tags color={ColorTags.RED}>NestJS</Tags>
+            <Tags color={ColorTags.LIME}>API Design</Tags>
+            <Tags color={ColorTags.SKY}>TypeScript</Tags>
+            <Tags color={ColorTags.ROSE}>Postgres</Tags>
           </>
         }
       />
